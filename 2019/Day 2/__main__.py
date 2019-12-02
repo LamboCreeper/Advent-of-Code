53f0131a141c65f733921b4f3940e97871fdb943
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-12-02 19:30:57 +0000

    Added Day 2 Part 1

---
+++ 2019/Day 2/__main__.py
@@ -0,0 +1,26 @@
+import os
+
+directory = os.path.dirname(os.path.realpath(__file__))
+data = open("{0}/data.txt".format(directory), "r").read()
+
+opcodes = [int(i) for i in data.split(",")]
+
+opcodes[1] = 12
+opcodes[2] = 2
+
+for i in range(0, len(opcodes), 4):
+    x = opcodes[i + 1]
+    y = opcodes[i + 2]
+    z = opcodes[i + 3]
+
+    if opcodes[i] == 1:
+        opcodes[z] = opcodes[x] + opcodes[y]
+
+    if opcodes[i] == 2:
+        opcodes[z] = opcodes[x] * opcodes[y]
+
+    if opcodes[i] == 99:
+        print(data)
+        print("".join(str("{0},".format(opcode)) for opcode in opcodes))
+
+        break
